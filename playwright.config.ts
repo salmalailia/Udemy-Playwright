ae47d8ac8c98d4574ddfb0658eae222f8d30f626
Traspac <salma.rahmalailia@gmail.com>
2026-02-26 13:32:21 +0700

    add report 3

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['line'], ['html', { outputFolder: 'tests-results', open: 'never' }], ['allure-playwright']],
+  reporter: [
+  ['line'], ['html', { outputFolder: 'playwright-report', open: 'never' }], ['json', { outputFile: 'test-results/results.json' }], ['allure-playwright']
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
   globalSetup: require.resolve('./utils/global-setup'),
